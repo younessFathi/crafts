21853f6615277a637794bda5448a1d8d593eeef5
unknown <theyounessfathi@gmail.com>
2019-09-01 20:50:00 +0100

    [ first commit develop ]

--- crafts-api/src/main/java/org/crafts/api/PersonController.java
+++ crafts-api/src/main/java/org/crafts/api/PersonController.java
@@ -4,13 +4,22 @@ import org.crafts.domain.DTO.PersonDTO;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * The Class PersonController.
+ */
 @RestController
 public class PersonController {
 	
+	/**
+	 * Gets the person.
+	 *
+	 * @return the person
+	 */
 	@GetMapping("/person")
 	public String getPerson() {
 		PersonDTO personDTO = new PersonDTO();
 		return personDTO.toString();
 	}
+	
 
 }
